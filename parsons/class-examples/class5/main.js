86901fd027979e8dd7c8336f838d12bb1f883b79
agnescameron <agnesfcameron@gmail.com>
2020-02-21 17:40:25 -0500

    added stray script tag

--- parsons/class-examples/class5/main.js
+++ parsons/class-examples/class5/main.js
@@ -1,3 +1,8 @@
-function printWord (word) {
+function printWord(word) {
 	console.log(word);
+}
+
+function changeHeading() {
+	document.getElementById('heading').style.backgroundColor='lightblue';
+	document.getElementById('heading').innerHTML = "I clicked the button"
 }
