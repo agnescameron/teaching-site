5c23e67a8174d926b226a7088e4dddf93ed78ad7
agnescameron <agnesfcameron@gmail.com>
2020-02-28 15:56:45 -0500

    class 6

---
+++ parsons/class-examples/class6/main.js
@@ -0,0 +1,14 @@
+function changeHeading () {
+	$('#heading').css({"background-color": "yellow"})
+}
+
+
+window.onload = function() {
+
+	$("p").click(function(){
+	  $("p").css({"background-color": "pink"});
+	});
+
+	$(".green").css({"background-color": "lightgreen"});    
+
+}
