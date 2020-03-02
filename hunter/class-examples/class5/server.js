7e50c5507f40459046204c8518f86465c904f696
agnescameron <agnesfcameron@gmail.com>
2020-03-02 15:46:22 -0500

    added to demo

---
+++ hunter/class-examples/class5/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.json()) 
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.get('/about', function(req, res) {
+    res.send('<h1>About Page</h1><p>boo boo be doo</p>');
+});
+
+app.post('/login', function(req,res){
+  var user=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user+", password is "+password);
+  res.send(JSON.stringify("yes"));
+});
+
+app.listen(8080);
